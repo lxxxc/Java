73a7226887a08c1980d3217d10b52a7d41f26336
lxxxc <lxc115@126.com>
2017-10-17 12:57:47 +0800

    Update

---
+++ 50SpringFramework/51HelloWorld/src/com/spring/HelloWorld.java
@@ -0,0 +1,17 @@
+package com.spring;
+
+public class HelloWorld {
+	private String message;
+
+	public String getMessage() {
+		return this.message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+	public void printMessage() {
+		System.out.println("Your message: " + message);
+	}
+}
